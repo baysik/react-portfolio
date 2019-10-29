97794017213f169aee1a65f0239afdc2b636b820
baysik <iambaysik@gmail.com>
2019-10-29 10:17:01 -0600

    added hover effect to flexboxes

--- portfolio/src/components/Flexbox/Flexbox.js
+++ portfolio/src/components/Flexbox/Flexbox.js
@@ -3,27 +3,52 @@ import "./style.css";
 
 export default function Flexbox() {
   return (
-    <div className="container container-flex">
-      <div className="box-1">
-        <h3>Box one</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+    <div className="container">
+      <div className="container-flex">
+        <div className="boxes">
+          <h3>Box one</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="boxes">
+          <h3>Box two</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="boxes">
+          <h3>Box three</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </div>
-      <div className="box-2">
-        <h3>Box two</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-      </div>
-      <div className="box-3">
-        <h3>Box three</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+      <div className="container-flex">
+        <div className="boxes">
+          <h3>Box one</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="boxes">
+          <h3>Box two</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="boxes">
+          <h3>Box three</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </div>
     </div>
   );
