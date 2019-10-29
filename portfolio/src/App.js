efa07348c1954dc857fd75bc052f51d165bae7dd
baysik <iambaysik@gmail.com>
2019-10-28 20:24:16 -0600

    added about me

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
-// import Wrapper from "./components/Wrapper/Wrapper";
+import Wrapper from "./components/Wrapper/Wrapper";
 
 export default class App extends Component {
   render() {
@@ -10,6 +10,18 @@ export default class App extends Component {
       <div className="App">
         <Navbar />
         <Jumbotron />
+        <Wrapper>
+          <h2 className="aboutme">About me</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </Wrapper>
       </div>
     );
   }
