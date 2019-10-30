a11db2f6d391e4369b1366db24b063dea600d65e
baysik <iambaysik@gmail.com>
2019-10-30 15:16:54 -0600

    added animation to navbar

--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -1,46 +1,57 @@
-import React, { Component } from "react";
+import React, { Component, useRef, useEffect } from "react";
+import { TweenMax, Power3 } from "gsap";
 import "./style.css";
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="/">
-            GB
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="nav navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/portfolio">
-                  Portfolio
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+export default function Navbar() {
+  let navSlide = useRef(null);
+  let navSlide2 = useRef(null);
+  let navSlide3 = useRef(null);
+  useEffect(() => {
+    console.log(navSlide);
+    TweenMax.from([navSlide, navSlide2, navSlide3], 2, {
+      opacity: 1,
+      x: 200,
+      ease: Power3.easeOut,
+      delay: 0.2
+    });
+  }, []);
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">
+          GB
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="nav navbar-nav ml-auto">
+            <li className="nav-item" ref={el => (navSlide = el)}>
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item" ref={el => (navSlide2 = el)}>
+              <a className="nav-link" href="/portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item" ref={el => (navSlide3 = el)}>
+              <a className="nav-link" href="/contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
