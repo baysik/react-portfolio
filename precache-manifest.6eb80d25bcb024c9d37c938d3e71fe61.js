f211da570777a37501e13a845ac33c5368bd951e
baysik <iambaysik@gmail.com>
2021-03-20 20:14:37 -0600

    Updates

---
+++ precache-manifest.6eb80d25bcb024c9d37c938d3e71fe61.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b93d41bbb54965b6aa340fdbb13f671",
+    "url": "baysik.github.io/index.html"
+  },
+  {
+    "revision": "3c5cc2871605afe355d9",
+    "url": "baysik.github.io/static/css/main.8c8a818f.chunk.css"
+  },
+  {
+    "revision": "0a8053c8711382cd5eac",
+    "url": "baysik.github.io/static/js/2.5f6175e5.chunk.js"
+  },
+  {
+    "revision": "3c5cc2871605afe355d9",
+    "url": "baysik.github.io/static/js/main.5521f240.chunk.js"
+  },
+  {
+    "revision": "d2b6fc2fcda522ab5e76",
+    "url": "baysik.github.io/static/js/runtime-main.f7194f9d.js"
+  },
+  {
+    "revision": "bb46f717c3487d478cbb6f049cb153bb",
+    "url": "baysik.github.io/static/media/alphabet.bb46f717.png"
+  },
+  {
+    "revision": "0dbcd7635ae2df4aae2a7acb045ef3c1",
+    "url": "baysik.github.io/static/media/crystal collector.0dbcd763.png"
+  },
+  {
+    "revision": "a3344d3bb0e3131c2fe2cd4c97c12a91",
+    "url": "baysik.github.io/static/media/eattheburger.a3344d3b.png"
+  },
+  {
+    "revision": "422e496aa97959d4f1ef98a8d67dfa72",
+    "url": "baysik.github.io/static/media/friendfinder.422e496a.png"
+  },
+  {
+    "revision": "a43441a51471a923cadc25c2c72c0b18",
+    "url": "baysik.github.io/static/media/portrait.a43441a5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d6c8f2fec2121f9e66ef75a20aa5255.js"
+  "baysik.github.io/precache-manifest.6eb80d25bcb024c9d37c938d3e71fe61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("baysik.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
