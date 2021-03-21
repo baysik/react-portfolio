9b3350d7b69e73932579ac3b25d134a801d4a194
baysik <iambaysik@gmail.com>
2021-03-20 20:42:08 -0600

    Updates

---
+++ precache-manifest.8d0a37a7fbfcceb6197942eaea078077.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae894d87b920cbd9a2ac674512998ce7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcab5411f19220d7e895",
+    "url": "/static/css/main.8c8a818f.chunk.css"
+  },
+  {
+    "revision": "0e6502ec8445d18c13bb",
+    "url": "/static/js/2.e6c157a1.chunk.js"
+  },
+  {
+    "revision": "dcab5411f19220d7e895",
+    "url": "/static/js/main.fb3b19b3.chunk.js"
+  },
+  {
+    "revision": "0437ccf3fcfeaa1a5de9",
+    "url": "/static/js/runtime-main.c1483060.js"
+  },
+  {
+    "revision": "bb46f717c3487d478cbb6f049cb153bb",
+    "url": "/static/media/alphabet.bb46f717.png"
+  },
+  {
+    "revision": "0dbcd7635ae2df4aae2a7acb045ef3c1",
+    "url": "/static/media/crystal collector.0dbcd763.png"
+  },
+  {
+    "revision": "a3344d3bb0e3131c2fe2cd4c97c12a91",
+    "url": "/static/media/eattheburger.a3344d3b.png"
+  },
+  {
+    "revision": "422e496aa97959d4f1ef98a8d67dfa72",
+    "url": "/static/media/friendfinder.422e496a.png"
+  },
+  {
+    "revision": "a43441a51471a923cadc25c2c72c0b18",
+    "url": "/static/media/portrait.a43441a5.jpg"
+  }
+]);
