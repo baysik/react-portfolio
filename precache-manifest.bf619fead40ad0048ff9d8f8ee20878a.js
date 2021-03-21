241be2d66f2c2742f907cd240bbb8c23695f99fa
baysik <iambaysik@gmail.com>
2021-03-20 19:20:06 -0600

    Updates

---
+++ precache-manifest.bf619fead40ad0048ff9d8f8ee20878a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86974ae6ded62b6061ce4b24dac7cd2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "00b749741d75c4878be2",
+    "url": "/react-portfolio/static/css/main.8c8a818f.chunk.css"
+  },
+  {
+    "revision": "0e6502ec8445d18c13bb",
+    "url": "/react-portfolio/static/js/2.e6c157a1.chunk.js"
+  },
+  {
+    "revision": "00b749741d75c4878be2",
+    "url": "/react-portfolio/static/js/main.ee1bc03f.chunk.js"
+  },
+  {
+    "revision": "28cc23f26add4964e5fb",
+    "url": "/react-portfolio/static/js/runtime-main.be108fab.js"
+  },
+  {
+    "revision": "bb46f717c3487d478cbb6f049cb153bb",
+    "url": "/react-portfolio/static/media/alphabet.bb46f717.png"
+  },
+  {
+    "revision": "0dbcd7635ae2df4aae2a7acb045ef3c1",
+    "url": "/react-portfolio/static/media/crystal collector.0dbcd763.png"
+  },
+  {
+    "revision": "a3344d3bb0e3131c2fe2cd4c97c12a91",
+    "url": "/react-portfolio/static/media/eattheburger.a3344d3b.png"
+  },
+  {
+    "revision": "422e496aa97959d4f1ef98a8d67dfa72",
+    "url": "/react-portfolio/static/media/friendfinder.422e496a.png"
+  },
+  {
+    "revision": "a43441a51471a923cadc25c2c72c0b18",
+    "url": "/react-portfolio/static/media/portrait.a43441a5.jpg"
+  }
+]);
